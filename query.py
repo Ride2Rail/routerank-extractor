1ac392a47d4c8dd3323da2011262d07051a8a5e2
Cristian Consonni <cristian@balist.es>
2021-03-16 13:45:55 +0100

    Add example of querying data

---
+++ query.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+##############################################################################
+# Based on:
+#   https://github.com/RedisJSON/redisjson-py
+##############################################################################
+
+from pprint import pprint
+import rejson as rj
+
+
+if __name__ == '__main__':
+    redis = rj.Client(host='localhost', port=6379, decode_responses=True)
+
+    # getting a trip back
+    example = redis.jsonget('#25:10379', rj.Path.rootPath())
+
+    # import ipdb; ipdb.set_trace()
+
+    pprint(example)
+
+    exit(0)
