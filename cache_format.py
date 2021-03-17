fcda8410296e7a86c7b631cf36988eedbd0be791
Alex <alex.martinez@eurecat.org>
2021-03-17 09:59:38 +0100

    Update cache_format.py

--- cache_format.py
+++ cache_format.py
@@ -2,14 +2,10 @@
 # coding: utf-8
 
 
-#import modules
-import json
-#import pandas as pd
-import numpy as np
+# import modules
 from datetime import datetime, timedelta
-import pytz
-import re
 from geojson import MultiPoint
+
 
 def leg_type(mode):
     continuous_leg = ['walking', 'bike', 'car']
@@ -21,113 +17,117 @@ def leg_type(mode):
         return 'timed'
     if mode in ridesharing_leg:
         return 'ridesharing'
-    
-    
-def get_coordinates(offer,place):
-    lat, lon = offer['places'][place]['latitude'],offer['places'][place]['longitude']
-    return (float(lat),float(lon))
 
-def GetTime_format(dur):
+
+def get_coordinates(offer, place):
+    lat, lon = offer['places'][place]['latitude'], offer['places'][place]['longitude']
+    return float(lat), float(lon)
+
+
+def get_time_format(dur):
     sec = timedelta(seconds=dur)
-    d = datetime(10,10,10) + sec
-    return 'P%dY%dM%dDT%dH%dM%dS' % (d.year-10,d.month-10,d.day-10, d.hour, d.minute, d.second)
+    d = datetime(10, 10, 10) + sec
+    return 'P%dY%dM%dDT%dH%dM%dS' % (d.year - 10, d.month - 10, d.day - 10, d.hour, d.minute, d.second)
+
 
 def transform_trip(trip):
     request_id = str(trip['legId'])
     example_new_format = dict()
-    example_new_format.setdefault(request_id,{})
-    FMT = '%H:%M'
+    example_new_format.setdefault(request_id, {})
+    fmt = '%H:%M'
+    offer_list = []
     for alternative in trip['alternatives']:
-        #offer level information
-        #get the offer ID and store it
+        # offer level information
+        # get the offer ID and store it
         offer_id = alternative['id']
-        example_new_format[request_id].setdefault(offer_id,{})
+        offer_list.append(offer_id)
+        example_new_format[request_id].setdefault(offer_id, {})
 
-        #get the starting time and store it
+        # get the starting time and store it
         start_time = alternative['segments'][0]['departureTime']
         start_date = alternative['segments'][0]['departureDate']
-        t = start_date+' '+start_time
+        t = start_date + ' ' + start_time
         starttime = datetime.fromisoformat(t).isoformat()
-        example_new_format[request_id][offer_id].setdefault('start_time',starttime)
+        example_new_format[request_id][offer_id].setdefault('start_time', starttime)
 
-        #get the ending time and store it
-        end_time = alternative['segments'][len(alternative['segments'])-1]['arrivalTime']
-        end_date = alternative['segments'][len(alternative['segments'])-1]['arrivalDate']
-        t = end_date+' '+end_time
+        # get the ending time and store it
+        end_time = alternative['segments'][len(alternative['segments']) - 1]['arrivalTime']
+        end_date = alternative['segments'][len(alternative['segments']) - 1]['arrivalDate']
+        t = end_date + ' ' + end_time
         endtime = datetime.fromisoformat(t).isoformat()
-        example_new_format[request_id][offer_id].setdefault('end_time',endtime)
+        example_new_format[request_id][offer_id].setdefault('end_time', endtime)
 
-        #get duration and store it
-        tdelta = datetime.strptime(end_time,FMT)-datetime.strptime(start_time,FMT)
-        example_new_format[request_id][offer_id].setdefault('duration', GetTime_format(tdelta.total_seconds()))
-        #num_legs = 0
+        # get duration and store it
+        tdelta = datetime.strptime(end_time, fmt) - datetime.strptime(start_time, fmt)
+        example_new_format[request_id][offer_id].setdefault('duration', get_time_format(tdelta.total_seconds()))
+        # num_legs = 0
         for segment in alternative['segments']:
-            #num_legs += len(segment['legs'])
+            # num_legs += len(segment['legs'])
             leg_ids = []
             for leg in segment['legs']:
                 leg_ids.append(leg['id'])
-        example_new_format[request_id][offer_id].setdefault('num_interchanges', len(leg_ids)-1)
-        example_new_format[request_id][offer_id].setdefault('legs',leg_ids)
-        #offer_items
+        example_new_format[request_id][offer_id].setdefault('num_interchanges', len(leg_ids) - 1)
+        example_new_format[request_id][offer_id].setdefault('legs', leg_ids)
+        # offer_items
         example_new_format[request_id][offer_id].setdefault('offer_items', )
-        #bookable_total
+        # bookable_total
         example_new_format[request_id][offer_id].setdefault('bookable_total', )
-        #get the price in the correct format (last two digits are decimal)
-        price = int(float(alternative['totals']['price'])*100)
+        # get the price in the correct format (last two digits are decimal)
+        price = int(float(alternative['totals']['price']) * 100)
         example_new_format[request_id][offer_id].setdefault('complete_total', price)
-        #currency (always EUR in the case of routeRANK data)
+        # currency (always EUR in the case of routeRANK data)
         example_new_format[request_id][offer_id].setdefault('currency', 'EUR')
-        #leg level information
+        # leg level information
         for segment in alternative['segments']:
             for leg in segment['legs']:
                 leg_id = leg['id']
-                #leg type
+                # leg type
                 example_new_format[request_id][offer_id].setdefault(leg_id, {})
                 legtype = leg_type(leg['transport'])
-                example_new_format[request_id][offer_id][leg_id].setdefault('leg_type',legtype)
+                example_new_format[request_id][offer_id][leg_id].setdefault('leg_type', legtype)
 
-                #departure time
+                # departure time
                 start_time = leg['departureTime']
                 start_date = leg['departureDate']
-                t = start_date+' '+start_time
+                t = start_date + ' ' + start_time
                 starttime = datetime.fromisoformat(t).isoformat()
-                example_new_format[request_id][offer_id][leg_id].setdefault('start_time',starttime)
+                example_new_format[request_id][offer_id][leg_id].setdefault('start_time', starttime)
 
-                #arrival time
+                # arrival time
                 end_time = leg['arrivalTime']
                 end_date = leg['arrivalDate']
-                t = end_date+' '+end_time
+                t = end_date + ' ' + end_time
                 endtime = datetime.fromisoformat(t).isoformat()
-                example_new_format[request_id][offer_id][leg_id].setdefault('end_time',endtime)
+                example_new_format[request_id][offer_id][leg_id].setdefault('end_time', endtime)
 
-                #duration
-                tdelta = datetime.strptime(end_time,FMT)-datetime.strptime(start_time,FMT)
-                #print(tdelta.total_seconds())
-                example_new_format[request_id][offer_id][leg_id].setdefault('duration', GetTime_format(tdelta.total_seconds()))
-                #transportation mode
-                example_new_format[request_id][offer_id][leg_id].setdefault('transportation_mode',leg['transport'])
+                # duration
+                tdelta = datetime.strptime(end_time, fmt) - datetime.strptime(start_time, fmt)
+                # print(tdelta.total_seconds())
+                example_new_format[request_id][offer_id][leg_id].setdefault('duration',
+                                                                            get_time_format(tdelta.total_seconds()))
+                # transportation mode
+                example_new_format[request_id][offer_id][leg_id].setdefault('transportation_mode', leg['transport'])
 
-                #leg stops
-                coord_ini = get_coordinates(trip,leg['from'])
-                coord_fin = get_coordinates(trip,leg['to'])
-                #example_new_format[request_id][offer_id][leg_id].setdefault('leg_stops',[coord_ini,coord_fin])
-                example_new_format[request_id][offer_id][leg_id].setdefault('leg_stops',MultiPoint([coord_ini,coord_fin]))
+                # leg stops
+                coord_ini = get_coordinates(trip, leg['from'])
+                coord_fin = get_coordinates(trip, leg['to'])
+                # example_new_format[request_id][offer_id][leg_id].setdefault('leg_stops',[coord_ini,coord_fin])
+                example_new_format[request_id][offer_id][leg_id].setdefault('leg_stops',
+                                                                            MultiPoint([coord_ini, coord_fin]))
 
-                #leg track
-                example_new_format[request_id][offer_id][leg_id].setdefault('leg_track',)
+                # leg track
+                example_new_format[request_id][offer_id][leg_id].setdefault('leg_track', )
 
-                #travel_expert
-                example_new_format[request_id][offer_id][leg_id].setdefault('travel_expert',)
+                # travel_expert
+                example_new_format[request_id][offer_id][leg_id].setdefault('travel_expert', )
 
-                #line and journey
-                if (legtype == 'timed'):
-                    example_new_format[request_id][offer_id][leg_id].setdefault('line',)
-                    example_new_format[request_id][offer_id][leg_id].setdefault('journey',)
-                if (legtype == 'ridesharing'):
-                    example_new_format[request_id][offer_id][leg_id].setdefault('driver',)
-                    example_new_format[request_id][offer_id][leg_id].setdefault('passenger',)
-                    example_new_format[request_id][offer_id][leg_id].setdefault('vehicle',)
-                    
+                # line and journey
+                if legtype == 'timed':
+                    example_new_format[request_id][offer_id][leg_id].setdefault('line', )
+                    example_new_format[request_id][offer_id][leg_id].setdefault('journey', )
+                if legtype == 'ridesharing':
+                    example_new_format[request_id][offer_id][leg_id].setdefault('driver', )
+                    example_new_format[request_id][offer_id][leg_id].setdefault('passenger', )
+                    example_new_format[request_id][offer_id][leg_id].setdefault('vehicle', )
+        example_new_format[request_id].setdefault('offers', offer_list)
     return example_new_format
-
-
