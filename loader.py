895dcf53affb1a9264cfa9161b27e17a14aaef0e
Cristian Consonni <cristian@balist.es>
2021-03-24 23:53:13 +0100

    Fix loader

--- loader.py
+++ loader.py
@@ -7,7 +7,7 @@
 import argparse
 import json
 import itertools
-import rejson as rj
+import redis
 from typing import IO, Union
 
 import compressed_stream as cs
@@ -39,98 +39,152 @@ if __name__ == '__main__':
         json_files.append(open_jsonobjects_file(json_file))
     json_files = itertools.chain.from_iterable(json_files)
 
-    redis = rj.Client(host='localhost', port=6379, decode_responses=True)
+    redis = redis.Redis(host='localhost', port=6379)
 
     i = 1
     for json_file in json_files:
         for trip in json_file:
-            if len(trip['alternatives']) != 0:
-                offer = transform_trip(trip)
+            if len(trip['alternatives']) > 0:
+                # get mobility request data
+                mreq = transform_trip(trip)
 
                 # request-level information
                 request_id = trip['legId']
-                redis.jsonset(request_id + ':offers', rj.Path.rootPath(), offer[request_id]['offers'])
 
-                if i % NPRINT == 0:
-                    print('insert #{} (request_id: {})'.format(i, request_id))
+                # insert a list
+                #   https://stackoverflow.com/a/39930371/2377454
+                redis.lpush('{request_id}:offers'.format(request_id=request_id),
+                            *mreq[request_id]['offers']
+                            )
 
                 # offer-level information
                 for alternative in trip['alternatives']:
                     offer_id = alternative['id']
+                    prefix = ('{request_id}:{offer_id}'
+                              .format(request_id=request_id,
+                                      offer_id=offer_id)
+                              )
+
                     # start_time
-                    redis.jsonset(request_id + ':' + offer_id + ':start_time', rj.Path.rootPath(),
-                                  offer[request_id][offer_id]['start_time'])
+                    redis.set('{p}:start_time'.format(p=prefix),
+                              mreq[request_id][offer_id]['start_time']
+                              )
                     # end_time
-                    redis.jsonset(request_id + ':' + offer_id + ':end_time', rj.Path.rootPath(),
-                                  offer[request_id][offer_id]['end_time'])
+                    redis.set('{p}:end_time'.format(p=prefix),
+                              mreq[request_id][offer_id]['end_time']
+                              )
                     # duration
-                    redis.jsonset(request_id + ':' + offer_id + ':duration', rj.Path.rootPath(),
-                                  offer[request_id][offer_id]['duration'])
+                    redis.set('{p}:duration'.format(p=prefix),
+                              mreq[request_id][offer_id]['duration']
+                              )
                     # num_interchanges
-                    redis.jsonset(request_id + ':' + offer_id + ':num_interchanges', rj.Path.rootPath(),
-                                  offer[request_id][offer_id]['num_interchanges'])
+                    redis.set('{p}:num_interchanges'.format(p=prefix),
+                              mreq[request_id][offer_id]['num_interchanges']
+                              )
+
                     # legs
-                    redis.jsonset(request_id + ':' + offer_id + ':legs', rj.Path.rootPath(),
-                                  offer[request_id][offer_id]['legs'])
+                    if mreq[request_id][offer_id]['legs']:
+                        redis.lpush('{p}:legs'.format(p=prefix),
+                                    *mreq[request_id][offer_id]['legs']
+                                    )
+
                     # offer_items
-                    redis.jsonset(request_id + ':' + offer_id + ':offer_items', rj.Path.rootPath(),
-                                  offer[request_id][offer_id]['offer_items'])
+                    if mreq[request_id][offer_id]['offer_items']:
+                        redis.lpush('{p}:offer_items'.format(p=prefix),
+                                    *mreq[request_id][offer_id]['offer_items']
+                                    )
+
                     # bookable_total
-                    redis.jsonset(request_id + ':' + offer_id + ':bookable_total', rj.Path.rootPath(),
-                                  offer[request_id][offer_id]['bookable_total'])
+                    if mreq[request_id][offer_id]['bookable_total']:
+                        redis.set('{p}:bookable_total'.format(p=prefix),
+                                  mreq[request_id][offer_id]['bookable_total']
+                                  )
+
                     # complete_total
-                    redis.jsonset(request_id + ':' + offer_id + ':complete_total', rj.Path.rootPath(),
-                                  offer[request_id][offer_id]['complete_total'])
+                    redis.set('{p}:complete_total'.format(p=prefix),
+                              mreq[request_id][offer_id]['complete_total']
+                              )
                     # currency
-                    redis.jsonset(request_id + ':' + offer_id + ':currency', rj.Path.rootPath(),
-                                  offer[request_id][offer_id]['currency'])
+                    redis.set('{p}:currency'.format(p=prefix),
+                              mreq[request_id][offer_id]['currency']
+                              )
 
                     # leg-level information
                     for segment in alternative['segments']:
                         for leg in segment['legs']:
                             leg_id = leg['id']
                             # leg_type
-                            redis.jsonset(request_id + ':' + offer_id + ':' + leg_id + ':' + 'leg_type',
-                                          rj.Path.rootPath(), offer[request_id][offer_id][leg_id]['leg_type'])
+                            prefix_leg = ('{request_id}:{offer_id}:{leg_id}'
+                                          .format(request_id=request_id,
+                                                  offer_id=offer_id,
+                                                  leg_id=leg_id)
+                                          )
+                            redis.set('{pl}:leg_type'.format(pl=prefix_leg),
+                                      mreq[request_id][offer_id][leg_id]['leg_type']
+                                      )
                             # start_time
-                            redis.jsonset(request_id + ':' + offer_id + ':' + leg_id + ':' + 'start_time',
-                                          rj.Path.rootPath(), offer[request_id][offer_id][leg_id]['start_time'])
+                            redis.set('{pl}:start_time'.format(pl=prefix_leg),
+                                      mreq[request_id][offer_id][leg_id]['start_time']
+                                      )
                             # end_time
-                            redis.jsonset(request_id + ':' + offer_id + ':' + leg_id + ':' + 'end_time',
-                                          rj.Path.rootPath(), offer[request_id][offer_id][leg_id]['end_time'])
+                            redis.set('{pl}:end_time'.format(pl=prefix_leg),
+                                      mreq[request_id][offer_id][leg_id]['end_time']
+                                      )
                             # duration
-                            redis.jsonset(request_id + ':' + offer_id + ':' + leg_id + ':' + 'duration',
-                                          rj.Path.rootPath(), offer[request_id][offer_id][leg_id]['duration'])
+                            redis.set('{pl}:duration'.format(pl=prefix_leg),
+                                      mreq[request_id][offer_id][leg_id]['duration']
+                                      )
                             # transportation_mode
-                            redis.jsonset(request_id + ':' + offer_id + ':' + leg_id + ':' + 'transportation_mode',
-                                          rj.Path.rootPath(),
-                                          offer[request_id][offer_id][leg_id]['transportation_mode'])
+                            redis.set('{pl}:transportation_mode'.format(pl=prefix_leg),
+                                      mreq[request_id][offer_id][leg_id]['transportation_mode']
+                                      )
                             # leg_stops
-                            redis.jsonset(request_id + ':' + offer_id + ':' + leg_id + ':' + 'leg_stops',
-                                          rj.Path.rootPath(), offer[request_id][offer_id][leg_id]['leg_stops'])
+                            redis.set('{pl}:leg_stops'.format(pl=prefix_leg),
+                                      json.dumps(mreq[request_id][offer_id][leg_id]['leg_stops'])
+                                      )
                             # leg_track
-                            redis.jsonset(request_id + ':' + offer_id + ':' + leg_id + ':' + 'leg_track',
-                                          rj.Path.rootPath(), offer[request_id][offer_id][leg_id]['leg_track'])
+                            if mreq[request_id][offer_id][leg_id]['leg_track']:
+                                redis.set('{pl}:leg_track'.format(pl=prefix_leg),
+                                          mreq[request_id][offer_id][leg_id]['leg_track']
+                                          )
+
                             # travel_expert
-                            redis.jsonset(request_id + ':' + offer_id + ':' + leg_id + ':' + 'travel_expert',
-                                          rj.Path.rootPath(), offer[request_id][offer_id][leg_id]['travel_expert'])
-                            if offer[request_id][offer_id][leg_id]['leg_type'] == 'timed':
+                            if mreq[request_id][offer_id][leg_id]['travel_expert']:
+                                redis.set('{pl}:travel_expert'.format(pl=prefix_leg),
+                                          mreq[request_id][offer_id][leg_id]['travel_expert']
+                                          )
+
+                            if mreq[request_id][offer_id][leg_id]['leg_type'] == 'timed':
                                 # line
-                                redis.jsonset(request_id + ':' + offer_id + ':' + leg_id + ':' + 'line',
-                                              rj.Path.rootPath(), offer[request_id][offer_id][leg_id]['line'])
+                                if mreq[request_id][offer_id][leg_id]['line']:
+                                    redis.set('{pl}:line'.format(pl=prefix_leg),
+                                              mreq[request_id][offer_id][leg_id]['line']
+                                              )
                                 # journey
-                                redis.jsonset(request_id + ':' + offer_id + ':' + leg_id + ':' + 'journey',
-                                              rj.Path.rootPath(), offer[request_id][offer_id][leg_id]['journey'])
-                            elif offer[request_id][offer_id][leg_id]['leg_type'] == 'ridesharing':
+                                if mreq[request_id][offer_id][leg_id]['journey']:
+                                    redis.set('{pl}:journey'.format(pl=prefix_leg),
+                                              mreq[request_id][offer_id][leg_id]['journey']
+                                              )
+
+                            elif mreq[request_id][offer_id][leg_id]['leg_type'] == 'ridesharing':
                                 # driver
-                                redis.jsonset(request_id + ':' + offer_id + ':' + leg_id + ':' + 'driver',
-                                              rj.Path.rootPath(), offer[request_id][offer_id][leg_id]['driver'])
+                                if mreq[request_id][offer_id][leg_id]['driver']:
+                                    redis.set('{pl}:driver'.format(pl=prefix_leg),
+                                              mreq[request_id][offer_id][leg_id]['driver']
+                                              )
                                 # passenger
-                                redis.jsonset(request_id + ':' + offer_id + ':' + leg_id + ':' + 'passenger',
-                                              rj.Path.rootPath(), offer[request_id][offer_id][leg_id]['passenger'])
+                                if mreq[request_id][offer_id][leg_id]['passenger']:
+                                    redis.set('{pl}:passenger'.format(pl=prefix_leg),
+                                              mreq[request_id][offer_id][leg_id]['passenger']
+                                              )
                                 # vehicle
-                                redis.jsonset(request_id + ':' + offer_id + ':' + leg_id + ':' + 'vehicle',
-                                              rj.Path.rootPath(), offer[request_id][offer_id][leg_id]['vehicle'])
+                                if mreq[request_id][offer_id][leg_id]['vehicle']:
+                                    redis.set('{pl}:vehicle'.format(pl=prefix_leg),
+                                              mreq[request_id][offer_id][leg_id]['vehicle']
+                                              )
+
+                if i % NPRINT == 0:
+                    print('insert #{} (request_id: {})'.format(i, request_id))
 
                 i = i + 1
 
